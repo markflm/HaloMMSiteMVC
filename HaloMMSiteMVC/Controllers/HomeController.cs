b619e34abc15f8846789d348ff91d955df84c3f3
markflm <38789101+markflm@users.noreply.github.com>
2018-05-27 10:35:52 -0400

    added emblems
    added emblems

--- HaloMMSiteMVC/Controllers/HomeController.cs
+++ HaloMMSiteMVC/Controllers/HomeController.cs
@@ -36,7 +36,10 @@ namespace HaloMMSiteMVC.Controllers
             List<int> intGameIDs1 = new List<int>();
             List<int> intGameIDs2 = new List<int>();
             List<int> matchedIDs = new List<int>();
-
+            List<int> gamesFromDB = new List<int>();
+            List<int> gamesToFetch = new List<int>();
+            List<Game> fullGamesFromDB = new List<Game>();
+            List<Game> fullGamesToDB = new List<Game>();
             
             Player playerOne = new Player((gt1.Trim()).ToUpper()); //trim any leading/trailing spaces since these would produce valid URLs
             Player playerTwo = new Player((gt2.Trim()).ToUpper());  //toUpper so string comparison below works
@@ -67,12 +70,22 @@ namespace HaloMMSiteMVC.Controllers
             //Grab GTs from textboxes
             //Check DB to see if players exist
 
-            if (db.IsInDB(playerOne.Name)) //if they do exist
+            if (db.IsInDBMM(playerOne.Name) && db.IsInDBCustom(playerOne.Name))
             {
-
                 intGameIDs1 = (db.ImportGamesFromDB(playerOne.Name, intGameIDs1, mmCheckBox, cusCheckBox));
                 playerOne.GameIDs = intGameIDs1;
-               
+
+            }
+            else if ((!(db.IsInDBMM(playerOne.Name)) && mmCheckBox) && !(cusCheckBox)) //if MM games aren't found in DB, MM box is checked and Custom Box isn't checked
+            { //get MM games only
+                playerOne.PopulateGameIDList(playerOne.Name, false);
+                db.AddPlayerToDB(playerOne.Name, playerOne.GameIDs, false); //runs "store MM games" proc
+
+            }
+            else if ((!(db.IsInDBCustom(playerOne.Name)) && cusCheckBox) && !(mmCheckBox)) //if custom games aren't found in DB, customs box is checked and MM box isn't
+            {
+                playerOne.PopulateGameIDList(playerOne.Name, true); //get customs
+                db.AddPlayerToDB(playerOne.Name, playerOne.GameIDs, true); //store customs
 
             }
             else //run bungie scraper to get games
@@ -104,10 +117,22 @@ namespace HaloMMSiteMVC.Controllers
             }
            
             //create player object, run the bungie scraper
-            if (db.IsInDB(playerTwo.Name))
+            if (db.IsInDBMM(playerTwo.Name) && db.IsInDBCustom(playerTwo.Name))
             {
                 intGameIDs2 = (db.ImportGamesFromDB(playerTwo.Name, intGameIDs2, mmCheckBox, cusCheckBox));
                 playerTwo.GameIDs = intGameIDs2;
+
+            }
+            else if((!(db.IsInDBMM(playerTwo.Name)) && mmCheckBox) && !(cusCheckBox)) //if MM games aren't found in DB, MM box is checked and Custom Box isn't checked
+            { //get MM games only
+                playerTwo.PopulateGameIDList(playerTwo.Name, false);
+                db.AddPlayerToDB(playerTwo.Name, playerTwo.GameIDs, false); //runs "store MM games" proc
+
+            }
+            else if ((!(db.IsInDBCustom(playerTwo.Name)) && cusCheckBox) && !(mmCheckBox)) //if custom games aren't found in DB, customs box is checked and MM box isn't
+            {
+                playerTwo.PopulateGameIDList(playerTwo.Name, true); //get customs
+                db.AddPlayerToDB(playerTwo.Name, playerTwo.GameIDs, true); //store customs
 
             }
             else //run bungie scraper to get games
@@ -143,16 +168,39 @@ namespace HaloMMSiteMVC.Controllers
             //Once both Player objects are populated, do list.intersect in controller to find matched games
 
             //run get game details
-            matchedIDs = (playerOne.GameIDs.Intersect(playerTwo.GameIDs)).ToList();
+            matchedIDs = (playerOne.GameIDs.Intersect(playerTwo.GameIDs)).ToList(); //matched GameIDs
+            //import from DB
+            gamesFromDB = db.ImportGameDetails(playerOne, matchedIDs);
+            //isolate matched IDs that weren't returned from DB
+            gamesToFetch = matchedIDs.Except(gamesFromDB).ToList();
+            //grab those from bungie
+            fullGamesFromDB = playerOne.GamesFromDB;
+            if (gamesToFetch.Count > 0) //if > 0 there are matched Games not in DB
+            {
+                //these are the games present in playerOne's GameList before the bungie fetch
+                                                      // i.e. games that don't need to be added to the DB
+
+                playerOne.GetMatchedGameDetails(gamesToFetch); //adds remaining games to playerOne's GameList
+
+                fullGamesToDB = playerOne.GameList.Except(fullGamesFromDB).ToList(); //isolates fetched games for addition to DB
+
+                //add those to DB
+                db.AddGameDetails(fullGamesToDB);
+            }
+            //else: (all matched games were in DB -- they're already added to playerOne.GameList from ImportGameDetails
+            
+            
+
             //await playerOne.GetMatchedGameDetailsAsync(matchedIDs);
-            playerOne.GetMatchedGameDetails(matchedIDs);
+
 
 
             playerOne.GameList.Sort((x, y) => DateTime.Compare(DateTime.Parse(x.Date), DateTime.Parse(y.Date))); //orders GameList by date of game ascending
             playerOne.GameList.Reverse(); //reverses the list (descending)
 
             ViewBag.Error = "";
-            playerOne.GetPlayerEmblem();
+            playerOne.EnemyName = playerTwo.Name;
+            playerOne.GetPlayerEmblem(playerTwo.Name);
             return View(playerOne);
         }
 
